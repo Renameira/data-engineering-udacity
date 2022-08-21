20c0aae19cc249d8d3d56e8ed2c2fc85bdd0f3f2
renaduarte <renato.duarte@squair.io>
2022-08-21 12:11:33 -0300

    Implement logging in create_table.py and etl.py.

--- data-warehouse-amazon-redshift/create_tables.py
+++ data-warehouse-amazon-redshift/create_tables.py
@@ -1,21 +1,34 @@
 from config import Settings
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("logs/debug-create-table.log"),
+        logging.StreamHandler(),
+    ],
+)
 
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
+        logging.info(f"{query}")
         conn.commit()
 
 
 def create_tables(cur, conn):
     for query in create_table_queries:
         cur.execute(query)
+        logging.info(f"{query}")
         conn.commit()
 
 
 def main():
+    logging.info(f"Process inicialized")
     conn = psycopg2.connect(
         "host={} dbname={} user={} password={} port={}".format(
             Settings.HOST,
@@ -32,6 +45,7 @@ def main():
     create_tables(cur, conn)
 
     conn.close()
+    logging.info(f"tables created")
 
 
 if __name__ == "__main__":
--- data-warehouse-amazon-redshift/etl.py
+++ data-warehouse-amazon-redshift/etl.py
@@ -1,21 +1,31 @@
 from config import Settings
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.FileHandler("logs/debug-etl.log"), logging.StreamHandler()],
+)
 
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
+        logging.info(f"{query}")
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
+        logging.info(f"{query}")
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    logging.info(f"Process inicialized")
     conn = psycopg2.connect(
         "host={} dbname={} user={} password={} port={}".format(
             Settings.HOST,
@@ -31,6 +41,8 @@ def main():
     insert_tables(cur, conn)
 
     conn.close()
+
+    logging.info(f"ETL finished")
 
 
 if __name__ == "__main__":
