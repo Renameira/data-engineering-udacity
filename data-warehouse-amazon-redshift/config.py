3184403b14eef33698298cb62fdb6cfb2dd00cc7
renaduarte <renato.duarte@squair.io>
2022-08-21 12:13:46 -0300

    Updated config.py with new variable

--- data-warehouse-amazon-redshift/config.py
+++ data-warehouse-amazon-redshift/config.py
@@ -1,16 +1,18 @@
 import configparser
 import os
 
-config = configparser.ConfigParser()
+
 current_dir = os.path.abspath(".")
 full_path = os.path.join(current_dir, "config/.env")
+
+config = configparser.ConfigParser()
 config.read(full_path, encoding="utf-8")
 
 
 class Settings:
     LOG_DATA = config.get("S3", "LOG_DATA")
     ARN = config.get("IAM_ROLE", "ARN")
-    REGION = config.get("IAM_ROLE", "REGION")
+    REGION = config.get("AWS", "REGION")
     LOG_JSON_PATH = config.get("S3", "LOG_JSONPATH")
     SONG_DATA = config.get("S3", "SONG_DATA")
     HOST = config.get("CLUSTER", "HOST")
@@ -18,3 +20,6 @@ class Settings:
     DB_USER = config.get("CLUSTER", "DB_USER")
     DB_PASSWORD = config.get("CLUSTER", "DB_PASSWORD")
     DB_PORT = config.get("CLUSTER", "DB_PORT")
+    KEY = config.get("AWS", "KEY")
+    SECRET = config.get("AWS", "SECRET")
+    TOKEN = config.get("AWS", "TOKEN")
